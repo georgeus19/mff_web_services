95d5192e6874868f157ee97fa632309042564798
Hrubý Kryštof <hrubyk97@gmail.com>
2022-03-31 17:38:31 +0200

    Working skeleton for BuyStocksProcess without external WS

--- 02-soap/custodian/src/main/java/custodian/Runner.java
+++ 02-soap/custodian/src/main/java/custodian/Runner.java
@@ -5,7 +5,7 @@ import javax.xml.ws.Endpoint;
 public class Runner {
 
 	public static void main(String[] args) {
-		Endpoint.publish("http://127.0.0.1:8000/Custodian", new CustodianImpl());
+		Endpoint.publish("http://127.0.0.1:8001/Custodian", new CustodianImpl());
 	}
 
 }
