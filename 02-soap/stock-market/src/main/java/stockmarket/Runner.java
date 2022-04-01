0523d31dab20a7a55f07e9c0c638a3d1cc191c12
Hrubý Kryštof <hrubyk97@gmail.com>
2022-04-01 14:29:15 +0200

    Added london stock market

--- 02-soap/stock-market/src/main/java/stockmarket/Runner.java
+++ 02-soap/stock-market/src/main/java/stockmarket/Runner.java
@@ -8,16 +8,28 @@ import javax.xml.ws.Endpoint;
 public class Runner {
 
 	public static void main(String[] args) {
-		StockMarket stockMarket = new StockMarketImpl(new StockList(
+		StockMarket amsterdamStockMarket = new StockMarketImpl(new StockList(
 			Arrays.asList(
-				new AvailableStock(new Stock(0, "Google", 2700), 4),
-				new AvailableStock(new Stock(1, "NVidia", 365), 10),
-				new AvailableStock(new Stock(2, "Microsoft", 300), 3),
-				new AvailableStock(new Stock(3, "AMD", 500), 32),
-				new AvailableStock(new Stock(4, "Tesla", 1200), 2)
+				new AvailableStock(new Stock(0, "Google", 2700), 200),
+				new AvailableStock(new Stock(1, "NVidia", 365), 102),
+				new AvailableStock(new Stock(2, "Microsoft", 300), 303),
+				new AvailableStock(new Stock(3, "AMD", 500), 323),
+				new AvailableStock(new Stock(4, "Tesla", 1200), 27)
 			)
 		));
 		
-		Endpoint.publish("http://127.0.0.1:8000/StockMarket", stockMarket);
+		StockMarket londonStockMarket = new StockMarketImpl(new StockList(
+				Arrays.asList(
+						new AvailableStock(new Stock(0, "Google", 2800), 200),
+						new AvailableStock(new Stock(1, "NVidia", 350), 195),
+						new AvailableStock(new Stock(2, "Microsoft", 310), 254),
+						new AvailableStock(new Stock(3, "AMD", 500), 322),
+						new AvailableStock(new Stock(4, "Tesla", 1150), 291)
+						)
+				));
+		Endpoint.publish("http://127.0.0.1:8000/StockMarket", amsterdamStockMarket);
+		
+		
+		Endpoint.publish("http://127.0.0.1:8002/StockMarket", londonStockMarket);
 	}
 }
